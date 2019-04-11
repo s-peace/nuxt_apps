602b2209d7f20e5fff3d91954228e8dec6924a0b
s-peace <ssmim777@gmail.com>
2019-04-12 02:07:41 +0900

    axios auth

--- pages/axios.vue
+++ pages/axios.vue
@@ -1,11 +1,12 @@
 <template>
   <section class="container">
     <h1>{{title}}</h1>
-    <p>{{message}}</p>
+    <h3>{{message}}</h3>
     <input v-model="find">
     <button @click="findKeyData">search_key</button>
     <button @click="findAgeData">search_age</button>
     <hr>
+    <h3>add_del data</h3>
     <table>
       <tr>
         <th>Email</th>
@@ -27,8 +28,10 @@
         <th></th>
         <td></td>
         <button @click="addData">Add data</button>
+        <button @click="delData">Delete data</button>
       </tr>
     </table>
+    <hr>
     <ul>
       <li>{{json_data}}</li>
     </ul>
@@ -42,6 +45,8 @@
 </template>
 
 <script>
+import firebase from "firebase"
+
 const axios = require('axios');
   let base_url = "https://nuxt-firebase-app-5b6cc.firebaseio.com/person";
   let query_key_url = ".json?orderBy=%22$key%22&equalTo=%22"
@@ -55,15 +60,15 @@ export default {
       username: '',
       tel: '',
       age: 0,
-      message: 'axios sample',
+      message: 'search data',
       find: '',
       json_data: {},
     };
   },
-  asyncData: async function(){
-    let result = await axios.get(base_url + '.json');
-    return {json_data: result.data};
-  },
+  // asyncData: async function(){
+  //   let result = await axios.get(base_url + '.json');
+  //   return {json_data: result.data};
+  // },
   methods: {
     findKeyData() {
       let find_url = base_url + query_key_url + this.find + '%22'
@@ -103,7 +108,17 @@ export default {
     },
     getAllData(){
       axios.get(base_url + '.json').then((res) => {
-        this.message = 'get all data'
+        // this.message = 'search data'
+        this.json_data = res.data
+      }).catch((e) => {
+        this.message = 'Error'
+        this.json_data = {}
+      })
+    },
+    delData() {
+      let del_url = base_url + '/' + this.email + '.json'
+
+      axios.delete(del_url).then((res) => {
         this.json_data = res.data
       }).catch((e) => {
         this.message = 'Error'
@@ -112,7 +127,22 @@ export default {
     }
   },
   created() {
-    this.getAllData()
+    let config = {
+      apiKey: "AIzaSyCCHSPN89TWX8IYpd5zRzSAytRhRDueMP0",
+      authDomain: "nuxt-firebase-app-5b6cc.firebaseapp.com",
+      databaseURL: "https://nuxt-firebase-app-5b6cc.firebaseio.com",
+      projectId: "nuxt-firebase-app-5b6cc",
+      storageBucket: "nuxt-firebase-app-5b6cc.appspot.com",
+      messagingSenderId: "836571126469"
+    }
+    firebase.initializeApp(config)
+
+    let provider = new firebase.auth.GoogleAuthProvider()
+    let self = this
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      self.message = result.user.displayName + ', ' + result.user.email
+    })
+    // this.getAllData()
   }
 }
 </script>
