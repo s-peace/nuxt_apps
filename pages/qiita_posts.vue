363418339cd57e51c47e4dfaf5f09812d68800ef
s-peace <ssmim777@gmail.com>
2019-04-16 14:31:42 +0900

    test build

--- pages/qiita_posts.vue
+++ pages/qiita_posts.vue
@@ -24,7 +24,7 @@
 <script>
 export default {
   async asyncData({app}){
-    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+    const items = await app.$axios.$get(`https://qiita.com/api/v2/items?query=tag:nuxt.js`)
     return {
       items
     }
