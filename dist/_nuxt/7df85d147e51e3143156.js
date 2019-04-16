9104f3f69bd1e8cee0f057aec1581099c748ba36
s-peace <ssmim777@gmail.com>
2019-04-16 14:33:27 +0900

    test build2

---
+++ dist/_nuxt/7df85d147e51e3143156.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{470:function(e,t,n){"use strict";n.r(t);var r=n(24),component=Object(r.a)({},function(){this.$createElement;this._self._c;return this._m(0)},[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("p",[this._v("\n    /users/register.vue\n  ")])])}],!1,null,null,null);t.default=component.exports}}]);
